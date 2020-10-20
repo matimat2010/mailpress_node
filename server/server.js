b9b9035bf49bf7a347400d92045f88a8aef6e678
Matt Alvarado <crpalvam@crp.leeent.net>
2020-10-20 12:51:07 -0500

    check out page is running

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
-// This example sets up an endpoint using the Express framework.
-// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 require('dotenv').config({path:'server/.env'});
-
-
-
 const express = require('express');
 const app = express();
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
 const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 const {resolve} = require('path');
+//body parser is a middle ware that helps parse the body of a request
+const bodyParser = require('body-parser');
 
 
 //retrieves stripe plans to console//
@@ -20,6 +15,8 @@ const {resolve} = require('path');
 // adding middle ware, takes a middle ware function that is passed a request response and then next
 //takes in the directory where our static files live
 app.use(express.static(process.env.STATIC_DIR));
+//installing bodyParser everywhere
+app.use(bodyParser.json());
 
 //Get route , takes a runction as a callback that will receive a request in a response. 'res.send'
 //sends a response back, response is the index.html page here 
@@ -37,6 +34,36 @@ app.get('/', (req,res) => {
 //this route resonds to request that come in through /public-keys
 app.get('/public-keys', (req, res) => {
   res.send({ key: process.env.STRIPE_PUBLIC_KEY}); 
+});
+
+
+// route that can recieve post request, gather info from client, 
+// below responds with the response body we recieve / echos it back
+// test post with curl -X POST localhost:2090/my-route -d '{"test":123}' -H "Content-Type: applocation/json"
+app.post('/my-route', (req, res) => {
+  console.log('body', req.body);
+  res.send(req.body);
+});
+
+//post route looks at data /request body that comes back and takes action (ex: add to database | api call etc)
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        // Replace `price_...` with the actual price ID for your subscription
+        // you created in step 2 of this guide.
+        price: 'price_1HZlUvA5YRGP79Zvuso00EE9',
+        quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    success_url: 'https://example.com/success',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+  res.json({ id: session.id });
 });
 
 
